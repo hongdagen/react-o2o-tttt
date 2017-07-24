f7dffc5dffe1793355c554052d82def8dc17d2e9
hongyi <570483535@qq.com>
2017-07-24 11:50:42 +0800

    update3

--- react-o2o/src/containers/City/index.jsx
+++ react-o2o/src/containers/City/index.jsx
@@ -1,6 +1,14 @@
 import React, { Component } from 'react';
 import PureRenderMixin from 'react-addons-pure-render-mixin'
 import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
+import * as userInfoActionsFromOtherFile from '../../actions/userinfo.js'
+import Header from '../../components/Header/index'
+import CurrentCity from '../../components/CurrentCity/index'
+import CityList from '../../components/CityList/index'
+import LocalStore from '../../util/localStore.js'
+import { CITYNAME } from '../../config/localStoreKey.js'
+import { withRouter } from 'react-router-dom'
 class City extends React.Component {
     constructor(props, context) {
         super(props, context)
@@ -9,21 +17,37 @@ class City extends React.Component {
     render() {
         return (
             <div>
-              <h1>city</h1>
+                <Header title="选择城市" />
+                <CurrentCity cityName={this.props.userinfo.cityName} />
+                <CityList changeFn={this.changeCity.bind(this)} />
             </div>
         )
+    }
+    changeCity(newCity) {
+        if (newCity == null) {
+            return
+        }
+        //修改redux
+        const userinfo = this.props.userinfo
+        userinfo.cityName = newCity
+        this.props.userInfoActions.update(userinfo)
+        //修改localstorage
+        LocalStore.setItem(CITYNAME, newCity)
+        //跳转到首页
+        this.props.history.push('/')
     }
 }
 
 function mapStateToProps(state) {
     return {
-        userinfo:state.userinfo
+        userinfo: state.userinfo
 
     }
 }
 function mapDispatchToProps(dispatch) {
     return {
-        }
+        userInfoActions: bindActionCreators(userInfoActionsFromOtherFile, dispatch)
+    }
 }
 export default connect(
     mapStateToProps,
